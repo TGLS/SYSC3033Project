b673d4092d85f5bf92053b44393a63af0f028c3e
Charlie Hardwick-Kelly <charliehardwickkelly@LABS.SCE.CARLETON.CA>
2018-03-04 14:28:27 -0500

    more updates onthe UI

--- one/src/main/java/intermediate/Intermediate.java
+++ one/src/main/java/intermediate/Intermediate.java
@@ -34,10 +34,6 @@ public class Intermediate {
 	}
 	
 
-	
-	
-	
-
 	private void intermediateInterface() {
 		//This method runs the Servers Command Line interface 
 		
@@ -48,6 +44,7 @@ public class Intermediate {
 		while (true) {
 			System.out.println("Please Enter Command >: ");
 			String commandIn = s.nextLine();
+			String[] commandParts = commandIn.split(" ");
 			commandIn.replaceAll("\\s", "");
 			
 			commandIn = commandIn.toLowerCase();
@@ -64,7 +61,6 @@ public class Intermediate {
 				System.out.println("You have entered Verbose Mode");
 				
 				
-				
 			//if quiet is entered, exit verbose mode.
 			}else if(commandIn.equals("quiet")) {
 				IntermediateControl.verboseMode = false;
@@ -73,51 +69,51 @@ public class Intermediate {
 				
 			// here we wan to be able to enter error modes	
 			//format of command:  mode [00][01][02][03][04]	Packet_type packet_number;
-			}else if(commandIn.contains("mode")){
+			}else if(commandParts[0].equals("mode")){
 				// now we need to validate the string format modes 
-				
 				String mode = "";
 				String packetType =""; 
 				int packetNumber ;
 				int specification; 
 				
-				String[] commandParts = commandIn.split(" ");
-				//ensure mode is the first item 
-				if(!(commandParts[0].equals("mode"))){
-					System.out.println("there is an error in your mode formating please try again mode must be at the fron of the command");
-					// print the proper formatting 
-				}else {
 				//ensure that packet type is valid
-					if(commandParts[1].equals("00")|commandParts[1].equals("01")|commandParts[1].equals("02")|commandParts[1].equals("03")) {
-						mode = commandParts[1]; 
-						if(commandParts[2].equals("ack")| commandParts[2].equals("data")|commandParts[2].equals("rrq")|commandParts[2].equals("wrq")){
-							packetType = commandParts[2];
-							// this will be the packet number  
-							if((commandParts[3].matches("[0-9]+"))){
-								packetNumber = Integer.parseInt(commandParts[2]); 
-								
-								// now need to validate the transferData example delay, number of duplications ... ect
-								if((commandParts[4].matches("[0-9]+"))){
-									specification = Integer.parseInt(commandParts[4]);
-									// if we get to here we have a valid imput 
-									IntermediateControl.mode = mode; 
-									IntermediateControl.packetType = packetType;
-									IntermediateControl.packetNumber = packetNumber; 
-									IntermediateControl.specification = specification;
-									
-								}else {
-									System.out.println("there is an error in your mode formating please try again ensure your SPECIFICATION is valid");
-								}
-								
+				if(commandParts[1].equals("00")|commandParts[1].equals("01")|commandParts[1].equals("02")|commandParts[1].equals("03") && commandParts.length ==5 ) {
+					mode = commandParts[1]; 
+					
+					
+					// need to ensure a proper length				
+					
+					if(commandParts[2].equals("ack")| commandParts[2].equals("data")|commandParts[2].equals("rrq")|commandParts[2].equals("wrq")){
+						packetType = commandParts[2];
+						System.out.println("In here3");
+						// this will be the packet number  
+						if((commandParts[3].matches("[0-9]+"))){
+							packetNumber = Integer.parseInt(commandParts[2]); 
+							System.out.println("In here4");
+							// now need to validate the transferData example delay, number of duplications ... ect
+							if((commandParts[4].matches("[0-9]+"))){
+								System.out.println("In here5");
+								specification = Integer.parseInt(commandParts[4]);
+								// if we get to here we have a valid imput 
+								IntermediateControl.mode = mode; 
+								IntermediateControl.packetType = packetType;
+								IntermediateControl.packetNumber = packetNumber; 
+								IntermediateControl.specification = specification;
 								
 							}else {
-								System.out.println("there is an error in your mode formating please try again ensure your PACKET_NUMBER is valid");
+								System.out.println("there is an error in your mode formating please try again ensure your SPECIFICATION is valid");
 							}
+							
+							
 						}else {
-							System.out.println("there is an error in your mode formating please try again ensure your PACKET_TYPE is valid");
+							System.out.println("there is an error in your mode formating please try again ensure your PACKET_NUMBER is valid");
 						}
+					}else {
+						System.out.println("there is an error in your mode formating please try again ensure your PACKET_TYPE is valid");
 					}
-				}	
+				}else {
+					System.out.println("Please try again");
+				}
 			//Not a valid Command  
 			}else {
 				System.out.println("Not a Valid Command Please try again");
